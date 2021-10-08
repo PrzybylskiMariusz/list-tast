203c45fe3b84779d1e53accbfa44a74f0df8188a
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2021-10-08 19:14:25 +0200

    Build state in store

--- src/components/CharacterList/component.js
+++ src/components/CharacterList/component.js
@@ -1,22 +1,18 @@
-import axios from 'axios'
-
-const API = 'https://rickandmortyapi.com/api/character'
-
 // @vue/component
+import { mapState } from 'vuex'
+
 export default {
   name: 'CharacterList',
   data () {
     return {
-      characters: []
     }
   },
   mounted () {
-    axios.get(API)
-      .then((response) => {
-        this.characters = response.data.results
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    this.$store.dispatch('loadCharackters')
+  },
+  computed: {
+    ...mapState([
+      'charackters'
+    ])
   }
 }
