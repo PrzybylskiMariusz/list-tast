2ed61f893886c9551e8c5a7c5386467f67d6d89a
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2021-10-08 14:26:41 +0200

    Base charachters list styling

--- src/components/CharacterList/index.vue
+++ src/components/CharacterList/index.vue
@@ -3,7 +3,13 @@
     CharacterList
     <ul>
       <li v-for="item in characters" :key="item.id">
-        <p>{{ item.name }}</p>
+        <p>{{ item.species }}</p>
+        <h2>{{ item.name }}</h2>
+        <img :src="item.image" />
+        <p><span>Status: </span>{{ item.status }}</p>
+        <p><span>Gender: </span>{{ item.gender }}</p>
+        <p><span>Created: </span>{{ item.created }}</p>
+        <p><span>Type: </span>{{ item.type }}</p>
       </li>
     </ul>
   </div>
